4e5edc97e272503b742180f02d76ed9d898469a2
ajfrierson <alvinfrierson@fuller.edu>
2019-01-28 14:59:13 -0800

    completed project.

--- todo/src/components/todoItem.js
+++ todo/src/components/todoItem.js
@@ -3,21 +3,21 @@ import { toggleComplete } from '../actions';
 import { connect } from 'react-redux';
 
 class TodoItem extends Component {
-    toggleComplete(event) {
-      this.props.toggleComplete(this.props.index);
-    }
-  
-    render() {
-      return (
-        <li>
-          <input type="checkbox" onChange={this.toggleComplete.bind(this)} />
-          {this.props.todo.value}
-        </li>
-      );
-    }
+  toggleComplete(event) {
+    this.props.toggleComplete(this.props.index);
   }
-  
-  export default connect(
-    null,
-    { toggleComplete }
-  )(TodoItem);
+
+  render() {
+    return (
+      <li>
+        <input type="checkbox" onChange={this.toggleComplete.bind(this)} />
+        {this.props.todo.value}
+      </li>
+    );
+  }
+}
+
+export default connect(
+  null,
+  { toggleComplete }
+)(TodoItem);
