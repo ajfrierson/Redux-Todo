c61f6c1b23f2248214887a553fef0e41fe76648b
ajfrierson <alvinfrierson@fuller.edu>
2019-01-28 14:30:09 -0800

    included addTodo event to app.js

--- todo/src/App.js
+++ todo/src/App.js
@@ -4,17 +4,25 @@ import { addTodo } from './actions';
 import TodoList from './components/TodoList';
 import './App.css';
 
-class components extends Component (
+class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       newTodo: ''
-    }
+    };
     this.addTodo = this.addTodo.bind(this);
     this.updateNewTodo = this.updateNewTodo.bind(this);
-  };
-)
-
+  }
+  addTodo(event) {
+    event.preventDefault();
+    this.props.addTodo({
+      value: this.state.newTodo,
+      complete: false
+    });
+    this.setState({
+      newTodo: ''
+    });
+  }
 
 
 
